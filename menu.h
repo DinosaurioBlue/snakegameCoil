c7a7e95a4000d9e21ede36e1f229f97f0f07ff21
Ana-dt <154478373+Ana-dt@users.noreply.github.com>
2024-08-20 11:46:07 -0300

    Update menu.h
    Complete menu

--- menu.h
+++ menu.h
@@ -2,10 +2,21 @@
 #define MENU
 
 void startGame(void);
-void checkPlayer(void);
+void checkPlayer(char);
 void signUp(void);
 void login(void);
+void top_score(void);
+int comparePlayer(const void *a, const void *b);
 
 #define NAME_MAX 30
+#define MAX_PLAYERS 150
+
+
+typedef struct{
+	char user[NAME_MAX];
+	int score;
+}player_t;
+
+
 
 #endif
