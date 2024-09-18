81c79396fde934f5c6442446f1e90602ea19fe98
Sacrilege001 <97389168+Sacrilege001@users.noreply.github.com>
2024-09-18 16:25:38 +0200

    Update backend.h

--- backend.h
+++ backend.h
@@ -1,37 +1,21 @@
-#ifndef BACKEND
-#define BACKEND
-
-
-typedef struct {
-    char snake_head;
-    char snake_body;
-    char board_char;
-    char fruit_ch;
-    unsigned int score;
-    unsigned int life;
-    unsigned int snake_length;
-    unsigned int board_height;
-    unsigned int board_width;
-    unsigned int fruit_x;
-    unsigned fruit_y;
-    unsigned int timestep;
-    unsigned int configured;
-} game_settings_t;
-
-typedef struct
-{
-    unsigned int x;
-    unsigned int y;
-}vector_t;
+#ifndef BACKEND_H
+#define BACKEND_H
 
 typedef struct {
-    vector_t * pos;
-    vector_t dir;
-    unsigned int length;
-} snake_t;
+    int x, y;
+} Position;
 
+// Snake game mechanics
+void initialize_game();
+void update_game_state();
+int check_game_over();
+Position* get_snake();
+int get_snake_length();
+Position get_food();
+void place_food();
 
-
-
+// Score management
+void increase_score();
+int get_score();
 
 #endif
