b855299176e5a24a9c97258351e9ecfa0af0ccf2
Sacrilege001 <97389168+Sacrilege001@users.noreply.github.com>
2024-09-18 16:26:07 +0200

    Update frontend.h

--- frontend.h
+++ frontend.h
@@ -1,3 +1,10 @@
-#ifndef FRONTEND
-#define FRONTEND
+#ifndef FRONTEND_H
+#define FRONTEND_H
+
+// Game UI
+void show_menu();
+void render_screen();
+void handle_input();
+void display_game_over();
+
 #endif
