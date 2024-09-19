3c3ade057f1860d5ec5820a11d23af082a9053a7
Ana-dt <154478373+Ana-dt@users.noreply.github.com>
2024-09-18 22:33:23 -0300

    new main

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include"snake.h"
-#include"menu.h"
-#include"score.h"
+#include"GENERAL.h"
+#include"BACKEND.h"
+#include"FRONTEND.h"
 #include<stdio.h>
 int main() {
     game_settings_t game;//instancia
