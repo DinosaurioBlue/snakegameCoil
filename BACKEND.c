aac9223170007b9b32e8a671e5c8e8d699b013ef
DinosaurioBlue <franco21bonacina07@gmail.com>
2024-09-19 19:37:53 -0300

    ncio

--- BACKEND.c
+++ BACKEND.c
@@ -127,47 +127,48 @@ void SpawnFruit(game_settings_t *game) {
 void MoveSnake(snake_t * snake, game_settings_t * game, bool * gameOver){
 
 	input_t direction;
-	while((direction= GetInput())!=NO_KEY)
-	switch (direction)
-	{
-	case UP: if (snake->dir.y != 1) {
-            snake->dir.x = 0;
-            snake->dir.y = -1;
-        }
-		break;
-	case DOWN: if (snake->dir.y != -1) {
-            snake->dir.x = 0;
-            snake->dir.y = 1;
-        }
-		break;
-	case LEFT: if (snake->dir.x != 1) {
-            snake->dir.x = -1;
-            snake->dir.y = 0;
-        }
-		break;
-	case RIGHT: if (snake->dir.x != -1) {
-            snake->dir.x = 1;
-            snake->dir.y = 0;
-        }
-		break;
-	case QUIT: *gameOver = 1;
-		break;
-	case PAUSE: 
-		while(GetInput()!=PAUSE);
-		break;
-	case RESTART:{ 
-			game->restarted =1;
-			FreeMem(snake->pos);//we free dinamic memory, bad things could happen if not
-			SetupBack(game);//will set the snake to it's initial setting
-			InitializeSnake(snake, game);
-			SpawnFruit(game);
-			
-		}
-		break;
-	default:
-		break;
+	while((direction= GetInput())!=NO_KEY){
+		switch (direction){
+			case UP: if (snake->dir.y != 1) {
+					snake->dir.x = 0;
+					snake->dir.y = -1;
+				}
+				break;
+			case DOWN: if (snake->dir.y != -1) {
+					snake->dir.x = 0;
+					snake->dir.y = 1;
+				}
+				break;
+			case LEFT: if (snake->dir.x != 1) {
+					snake->dir.x = -1;
+					snake->dir.y = 0;
+				}
+				break;
+			case RIGHT: if (snake->dir.x != -1) {
+					snake->dir.x = 1;
+					snake->dir.y = 0;
+				}
+				break;
+			case QUIT: *gameOver = 1;
+				break;
+			case PAUSE: 
+				while(GetInput()!=PAUSE);
+				break;
+			case RESTART:{ 
+					game->restarted =1;
+					FreeMem(snake->pos);//we free dinamic memory, bad things could happen if not
+					SetupBack(game);//will set the snake to it's initial setting
+					InitializeSnake(snake, game);
+					SpawnFruit(game);
+					
+				}
+				break;
+			default:
+				break;
 
-}
+		}
+	}
+	
 
 
 
