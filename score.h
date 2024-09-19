e7535f3658c0daa1ef9bd64695c0682495617d1b
DinosaurioBlue <franco21bonacina07@gmail.com>
2024-09-18 22:28:36 -0300

    ersaded

--- score.h
+++
@@ -1,7 +0,0 @@
-#ifndef USERNAME
-#define USERNAME
-#include"snake.h"
-void updateScore(game_settings_t *, const char *player);
-
-
-#endif
--- tempfranco.h
+++
@@ -1,3 +0,0 @@
-#include"GENERAL.h"
-void GameLoop(game_settings_t * game);
-void SetupBack(game_settings_t* game);
